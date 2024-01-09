b77f92e67a7ca9827ee052994f92eb53534487d7
Weston Pace <weston.pace@gmail.com>
2024-01-09 08:27:06 -0800

    chore: modify build process so substrait-expr could publish

--- substrait-expr/build.rs
+++ substrait-expr/build.rs
@@ -1,7 +1,7 @@
 use substrait_expr_funcgen::{generate_functions, Options};
 
 fn main() {
-    println!("cargo:rerun-if-changed=../substrait/extensions");
+    println!("cargo:rerun-if-changed=substrait/extensions");
     println!("cargo:rerun-if-changed=build.rs");
 
     let options = Options {
@@ -9,18 +9,23 @@ fn main() {
         ..Default::default()
     };
 
+    println!(
+        "cargo:warning={}",
+        std::env::current_dir().unwrap().display()
+    );
+
     generate_functions(&[(
         "https://github.com/substrait-io/substrait/blob/main/extensions/functions_arithmetic.yaml",
-        "../substrait/extensions/functions_arithmetic.yaml",
+        "substrait/extensions/functions_arithmetic.yaml",
     ), (
         "https://github.com/substrait-io/substrait/blob/main/extensions/functions_boolean.yaml",
-        "../substrait/extensions/functions_boolean.yaml",
+        "substrait/extensions/functions_boolean.yaml",
     ), (
         "https://github.com/substrait-io/substrait/blob/main/extensions/functions_comparison.yaml",
-        "../substrait/extensions/functions_comparison.yaml",
+        "substrait/extensions/functions_comparison.yaml",
     ), (
         "https://github.com/substrait-io/substrait/blob/main/extensions/functions_datetime.yaml",
-        "../substrait/extensions/functions_datetime.yaml",
+        "substrait/extensions/functions_datetime.yaml",
     )], options)
     .unwrap();
 }
