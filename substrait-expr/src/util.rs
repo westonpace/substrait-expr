b15ea3d238a5be6e5c6a834209bf7a0feafa095f
Tim Saucer <timsaucer@gmail.com>
2025-11-24 14:39:15 -0500

    Remove stale code

--- substrait-expr/src/util.rs
+++ substrait-expr/src/util.rs
@@ -2,26 +2,6 @@ use crate::error::{Result, SubstraitExprError};
 
 /// Helper trait for extracting a property that should always be present
 /// from a protobuf message and returning an error if it is not
-#[allow(dead_code)]
-pub(crate) trait HasRequiredProperties<T> {
-    fn into_required(self, prop_name: &str) -> Result<T>;
-}
-
-impl<T> HasRequiredProperties<T> for Option<T> {
-    // TODO: Is there any better way to do this that doesn't require specifying prop_name?
-    // Maybe a macro of some kind?
-    fn into_required(self, prop_name: &str) -> Result<T> {
-        self.ok_or_else(|| {
-            SubstraitExprError::InvalidSubstrait(format!(
-                "The required property {} is missing",
-                prop_name
-            ))
-        })
-    }
-}
-
-/// Helper trait for extracting a property that should always be present
-/// from a protobuf message and returning an error if it is not
 pub(crate) trait HasRequiredPropertiesRef<T> {
     fn required(&self, prop_name: &str) -> Result<&T>;
 }
